9546d7fed52b531bc0d2054d5537caaaea78030c
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-01-19 19:26:45 +0100

    Added basic train script

--- colorizers/eccv16.py
+++ colorizers/eccv16.py
@@ -1,75 +1,76 @@
 
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
 from IPython import embed
 
 from .base_color import *
+
 
 class ECCVGenerator(BaseColor):
     def __init__(self, norm_layer=nn.BatchNorm2d):
         super(ECCVGenerator, self).__init__()
 
-        model1=[nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=True),]
-        model1+=[nn.ReLU(True),]
-        model1+=[nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=True),]
-        model1+=[nn.ReLU(True),]
-        model1+=[norm_layer(64),]
+        model1 = [nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model1 += [nn.ReLU(True), ]
+        model1 += [nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=True), ]
+        model1 += [nn.ReLU(True), ]
+        model1 += [norm_layer(64), ]
 
-        model2=[nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=True),]
-        model2+=[nn.ReLU(True),]
-        model2+=[nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=True),]
-        model2+=[nn.ReLU(True),]
-        model2+=[norm_layer(128),]
+        model2 = [nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model2 += [nn.ReLU(True), ]
+        model2 += [nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=True), ]
+        model2 += [nn.ReLU(True), ]
+        model2 += [norm_layer(128), ]
 
-        model3=[nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=True),]
-        model3+=[nn.ReLU(True),]
-        model3+=[nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),]
-        model3+=[nn.ReLU(True),]
-        model3+=[nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=True),]
-        model3+=[nn.ReLU(True),]
-        model3+=[norm_layer(256),]
+        model3 = [nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model3 += [nn.ReLU(True), ]
+        model3 += [nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model3 += [nn.ReLU(True), ]
+        model3 += [nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=True), ]
+        model3 += [nn.ReLU(True), ]
+        model3 += [norm_layer(256), ]
 
-        model4=[nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=True),]
-        model4+=[nn.ReLU(True),]
-        model4+=[nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),]
-        model4+=[nn.ReLU(True),]
-        model4+=[nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),]
-        model4+=[nn.ReLU(True),]
-        model4+=[norm_layer(512),]
+        model4 = [nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model4 += [nn.ReLU(True), ]
+        model4 += [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model4 += [nn.ReLU(True), ]
+        model4 += [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model4 += [nn.ReLU(True), ]
+        model4 += [norm_layer(512), ]
 
-        model5=[nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),]
-        model5+=[nn.ReLU(True),]
-        model5+=[nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),]
-        model5+=[nn.ReLU(True),]
-        model5+=[nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),]
-        model5+=[nn.ReLU(True),]
-        model5+=[norm_layer(512),]
+        model5 = [nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True), ]
+        model5 += [nn.ReLU(True), ]
+        model5 += [nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True), ]
+        model5 += [nn.ReLU(True), ]
+        model5 += [nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True), ]
+        model5 += [nn.ReLU(True), ]
+        model5 += [norm_layer(512), ]
 
-        model6=[nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),]
-        model6+=[nn.ReLU(True),]
-        model6+=[nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),]
-        model6+=[nn.ReLU(True),]
-        model6+=[nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),]
-        model6+=[nn.ReLU(True),]
-        model6+=[norm_layer(512),]
+        model6 = [nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True), ]
+        model6 += [nn.ReLU(True), ]
+        model6 += [nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True), ]
+        model6 += [nn.ReLU(True), ]
+        model6 += [nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True), ]
+        model6 += [nn.ReLU(True), ]
+        model6 += [norm_layer(512), ]
 
-        model7=[nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),]
-        model7+=[nn.ReLU(True),]
-        model7+=[nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),]
-        model7+=[nn.ReLU(True),]
-        model7+=[nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),]
-        model7+=[nn.ReLU(True),]
-        model7+=[norm_layer(512),]
+        model7 = [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model7 += [nn.ReLU(True), ]
+        model7 += [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model7 += [nn.ReLU(True), ]
+        model7 += [nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model7 += [nn.ReLU(True), ]
+        model7 += [norm_layer(512), ]
 
-        model8=[nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True),]
-        model8+=[nn.ReLU(True),]
-        model8+=[nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),]
-        model8+=[nn.ReLU(True),]
-        model8+=[nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),]
-        model8+=[nn.ReLU(True),]
+        model8 = [nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True), ]
+        model8 += [nn.ReLU(True), ]
+        model8 += [nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model8 += [nn.ReLU(True), ]
+        model8 += [nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True), ]
+        model8 += [nn.ReLU(True), ]
 
-        model8+=[nn.Conv2d(256, 313, kernel_size=1, stride=1, padding=0, bias=True),]
+        model8 += [nn.Conv2d(256, 313, kernel_size=1, stride=1, padding=0, bias=True), ]
 
         self.model1 = nn.Sequential(*model1)
         self.model2 = nn.Sequential(*model2)
@@ -97,9 +98,18 @@ class ECCVGenerator(BaseColor):
 
         return self.unnormalize_ab(self.upsample4(out_reg))
 
-def eccv16(pretrained=True):
-	model = ECCVGenerator()
-	if(pretrained):
-		import torch.utils.model_zoo as model_zoo
-		model.load_state_dict(model_zoo.load_url('https://colorizers.s3.us-east-2.amazonaws.com/colorization_release_v2-9b330a0b.pth',map_location='cpu',check_hash=True))
-	return model
+
+def eccv16(pretrained=True, local_pth=None):
+    model = ECCVGenerator()
+
+    if (pretrained):
+        import torch.utils.model_zoo as model_zoo
+
+        if local_pth is None:
+            url = 'https://colorizers.s3.us-east-2.amazonaws.com/colorization_release_v2-9b330a0b.pth'
+            model.load_state_dict(model_zoo.load_url(url, map_location='cpu', check_hash=True))
+
+        else:  # Load from local path
+            model.load_state_dict(torch.load(local_pth, map_location='cpu'))
+
+    return model
