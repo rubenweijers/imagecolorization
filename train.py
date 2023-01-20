212de96ffc585d66a2fe0162a0c30e60476856e2
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-01-20 21:10:44 +0100

    Added postprocessing and better loss func

--- train.py
+++ train.py
@@ -1,15 +1,12 @@
 import argparse
 import os
 
-from tqmd import tqdm
+from tqdm import tqdm
 
 from colorizers import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--img_path', type=str, default='imgs/ansel_adams3.jpg')
 parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
-parser.add_argument('-o', '--save_prefix', type=str, default='saved',
-                    help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
 opt = parser.parse_args()
 
 # load colorizers
@@ -22,21 +19,25 @@ if (opt.use_gpu):
 
 
 # load training data from folder
-train_folder = './input/218/train/100/grey/'
+train_folder = './input/218/train/100/original/'
 
 # default size to process images is 256x256
 # grab L channel in both original ("orig") and resized ("rs") resolutions
 train_imgs = []
+train_imgs_l = []
+train_imgs_rs = []
 for img in os.listdir(train_folder):
-    img = Image.open(train_folder + img)
-    img = np.array(img)
+    img = load_img(train_folder + img)
+    img_pt = torch.from_numpy(img.copy()).float()
 
     (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
 
     if (opt.use_gpu):
         tens_l_rs = tens_l_rs.cuda()
 
-    train_imgs.append(tens_l_rs)
+    train_imgs.append(img_pt)
+    train_imgs_l.append(tens_l_orig)
+    train_imgs_rs.append(tens_l_rs)
 
 # Softmax loss
 loss_fn_eccv16 = torch.nn.MSELoss()
@@ -46,18 +47,27 @@ loss_fn_siggraph17 = torch.nn.MSELoss()
 opt_eccv16 = torch.optim.Adam(colorizer_eccv16.parameters(), lr=3.16e-5, betas=(0.9, 0.99), weight_decay=0.001)
 opt_siggraph17 = torch.optim.Adam(colorizer_siggraph17.parameters(), lr=3.16e-5, betas=(0.9, 0.99), weight_decay=0.001)
 
-train_imgs = train_imgs[:3]
+# train_imgs_rs = train_imgs_rs[:3]
 
 # Train loop
 for epoch in range(1):
-    for i, img in enumerate(tqdm(train_imgs)):
+    pbar = tqdm(train_imgs_rs)
+    for i, img in enumerate(pbar):
         # Forward pass
         out_eccv16 = colorizer_eccv16(img)
         out_siggraph17 = colorizer_siggraph17(img)
 
+        # Postprocess
+        out_eccv16 = postprocess_tens(train_imgs_l[i], out_eccv16)
+        out_siggraph17 = postprocess_tens(train_imgs_l[i], out_siggraph17)
+
+        # Convert to tensor that requires grad
+        out_eccv16 = torch.from_numpy(out_eccv16).float().requires_grad_()
+        out_siggraph17 = torch.from_numpy(out_siggraph17).float().requires_grad_()
+
         # Loss
-        loss_eccv16 = loss_fn_eccv16(out_eccv16, img)
-        loss_siggraph17 = loss_fn_siggraph17(out_siggraph17, img)
+        loss_eccv16 = loss_fn_eccv16(out_eccv16, train_imgs[i])
+        loss_siggraph17 = loss_fn_siggraph17(out_siggraph17, train_imgs[i])
 
         # Backward pass
         opt_eccv16.zero_grad()
@@ -69,7 +79,8 @@ for epoch in range(1):
         opt_siggraph17.step()
 
         # Rounded to 2 decimals
-        print(f"Epoch: {epoch}, Image: {i}, Loss ECCV16: {round(loss_eccv16.item(), 2)}, Loss SIGGRAPH17: {round(loss_siggraph17.item(), 2)}")
+        pbar.set_description(
+            f"Epoch: {epoch}, Image: {i}, Loss ECCV16: {round(loss_eccv16.item(), 2)}, Loss SIGGRAPH17: {round(loss_siggraph17.item(), 2)}")
 
 # Save models to file
 torch.save(colorizer_eccv16.state_dict(), 'colorizer_eccv16.pth')
